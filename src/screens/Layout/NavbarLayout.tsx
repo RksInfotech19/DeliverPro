250e11b90b44b7ed04d73e27a82cd791ebdcf3f8
Priyadharsani <rksinfotech18@gmail.com>
2025-07-01 17:39:18 +0530

    Made simple design in dashboard

--- src/screens/Layout/NavbarLayout.tsx
+++ src/screens/Layout/NavbarLayout.tsx
@@ -1,12 +1,27 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import DeliverProNavbar from '../Navbar/Navbar';
+import { ROLE_BASED_NAVIGATION } from '../Navbar/NavbarConfig';
 
 const Layout = () => {
+  const location = useLocation();
+
+  // Paths where the navbar should NOT show navItems
+  const noNavItemPaths = ['/', '/add-shop'];
+
+  // Check if current path should hide navItems
+  const shouldHideNavItems = noNavItemPaths.includes(location.pathname);
+
+  const userRole = 'ShopOwner'; // You can get this dynamically if needed
+
   return (
     <>
-      <DeliverProNavbar userRole="ShopOwner" userName="Priya" navItems={[]} />
+      <DeliverProNavbar
+        userRole={userRole}
+        userName="Priya"
+        navItems={shouldHideNavItems ? [] : ROLE_BASED_NAVIGATION[userRole].navItems}
+      />
       <main className='pageContent'>
-        <Outlet /> {/* This will render nested routes */}
+        <Outlet />
       </main>
     </>
   );
